6fec496145fd342409a4cd22408bbfa19b37e9f8
Mateus Sponton <mkays@pop-os.localdomain>
2023-03-26 19:17:45 -0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ async function bootstrap() {
   const app = await NestFactory.create(App);
   dotenv.config();
 
+  app.enableCors();
+
   const port = process.env.PORT || 3000;
 
   await app.listen(port, () => {
